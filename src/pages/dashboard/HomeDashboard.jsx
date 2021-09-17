7132b1209f3803dc571793c45b4b0647f264e71e
syamilAbdillah <abdillahsyamil77@gmail.com>
2021-09-17 08:40:45 +0700

    feat: add notif component

--- src/pages/dashboard/HomeDashboard.jsx
+++ src/pages/dashboard/HomeDashboard.jsx
@@ -1,49 +1,79 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Notification from '../../share/Notification'
 
 export default function HomeDashboard(props){
+	const [isNotifActive, setIsNotifActive] = useState(false)
+	const [timeoutID, setTimeoutID] = useState()
+
+	function closeNotif(){
+		setIsNotifActive(false)
+		clearTimeout(timeoutID)
+		console.log('close notif')
+	}
+
+	function showNotif(){
+		if(setIsNotifActive){
+			setIsNotifActive(false)
+			clearTimeout(timeoutID)
+		}
+		setIsNotifActive(true)
+		const currentTimeoutID = setTimeout(() => setIsNotifActive(false),3000)
+		setTimeoutID(currentTimeoutID)
+	}
 	return (
-		<h1 className="title is-1">
-			Home Dashboard
-			aalfskdfjlsakdjflaskdjflsakdfljaskdffffffffffffffffffffffffffffffff
-			fffffffffffffffffffffffffffff
-			Home Dashboard
-			aalfskdfjlsakdjflaskdjflsakdfljaskdffffffffffffffffffffffffffffffff
-			Home Dashboard
-			aalfskdfjlsakdjflaskdjflsakdfljaskdffffffffffffffffffffffffffffffff
-			fffffffffffffffffffffffffffff
-			Home Dashboard
-			aalfskdfjlsakdjflaskdjflsakdfljaskdffffffffffffffffffffffffffffffff
-			Home Dashboard
-			aalfskdfjlsakdjflaskdjflsakdfljaskdffffffffffffffffffffffffffffffff
-			fffffffffffffffffffffffffffff
-			Home Dashboard
-			aalfskdfjlsakdjflaskdjflsakdfljaskdffffffffffffffffffffffffffffffff
-			Home Dashboard
-			aalfskdfjlsakdjflaskdjflsakdfljaskdffffffffffffffffffffffffffffffff
-			fffffffffffffffffffffffffffff
-			Home Dashboard
-			aalfskdfjlsakdjflaskdjflsakdfljaskdffffffffffffffffffffffffffffffff
-			Home Dashboard
-			aalfskdfjlsakdjflaskdjflsakdfljaskdffffffffffffffffffffffffffffffff
-			fffffffffffffffffffffffffffff
-			Home Dashboard
-			aalfskdfjlsakdjflaskdjflsakdfljaskdffffffffffffffffffffffffffffffff
-			Home Dashboard
-			aalfskdfjlsakdjflaskdjflsakdfljaskdffffffffffffffffffffffffffffffff
-			fffffffffffffffffffffffffffff
-			Home Dashboard
-			aalfskdfjlsakdjflaskdjflsakdfljaskdffffffffffffffffffffffffffffffff
-			Home Dashboard
-			aalfskdfjlsakdjflaskdjflsakdfljaskdffffffffffffffffffffffffffffffff
-			fffffffffffffffffffffffffffff
-			Home Dashboard
-			aalfskdfjlsakdjflaskdjflsakdfljaskdffffffffffffffffffffffffffffffff
-			Home Dashboard
-			aalfskdfjlsakdjflaskdjflsakdfljaskdffffffffffffffffffffffffffffffff
-			fffffffffffffffffffffffffffff
-			Home Dashboard
-			aalfskdfjlsakdjflaskdjflsakdfljaskdffffffffffffffffffffffffffffffff
-			{import.meta.env.MODE}
-		</h1>
+		<>
+			<Notification
+				message="f*cking Notification"
+				status="success"
+				isActive={isNotifActive}
+				inActive={closeNotif}
+			/>
+			<button onClick={showNotif} className="button is-primary">
+				show Notification !!
+			</button>
+			<h1 className="title is-1">
+				Home Dashboard
+				aalfskdfjlsakdjflaskdjflsakdfljaskdffffffffffffffffffffffffffffffff
+				fffffffffffffffffffffffffffff
+				Home Dashboard
+				aalfskdfjlsakdjflaskdjflsakdfljaskdffffffffffffffffffffffffffffffff
+				Home Dashboard
+				aalfskdfjlsakdjflaskdjflsakdfljaskdffffffffffffffffffffffffffffffff
+				fffffffffffffffffffffffffffff
+				Home Dashboard
+				aalfskdfjlsakdjflaskdjflsakdfljaskdffffffffffffffffffffffffffffffff
+				Home Dashboard
+				aalfskdfjlsakdjflaskdjflsakdfljaskdffffffffffffffffffffffffffffffff
+				fffffffffffffffffffffffffffff
+				Home Dashboard
+				aalfskdfjlsakdjflaskdjflsakdfljaskdffffffffffffffffffffffffffffffff
+				Home Dashboard
+				aalfskdfjlsakdjflaskdjflsakdfljaskdffffffffffffffffffffffffffffffff
+				fffffffffffffffffffffffffffff
+				Home Dashboard
+				aalfskdfjlsakdjflaskdjflsakdfljaskdffffffffffffffffffffffffffffffff
+				Home Dashboard
+				aalfskdfjlsakdjflaskdjflsakdfljaskdffffffffffffffffffffffffffffffff
+				fffffffffffffffffffffffffffff
+				Home Dashboard
+				aalfskdfjlsakdjflaskdjflsakdfljaskdffffffffffffffffffffffffffffffff
+				Home Dashboard
+				aalfskdfjlsakdjflaskdjflsakdfljaskdffffffffffffffffffffffffffffffff
+				fffffffffffffffffffffffffffff
+				Home Dashboard
+				aalfskdfjlsakdjflaskdjflsakdfljaskdffffffffffffffffffffffffffffffff
+				Home Dashboard
+				aalfskdfjlsakdjflaskdjflsakdfljaskdffffffffffffffffffffffffffffffff
+				fffffffffffffffffffffffffffff
+				Home Dashboard
+				aalfskdfjlsakdjflaskdjflsakdfljaskdffffffffffffffffffffffffffffffff
+				Home Dashboard
+				aalfskdfjlsakdjflaskdjflsakdfljaskdffffffffffffffffffffffffffffffff
+				fffffffffffffffffffffffffffff
+				Home Dashboard
+				aalfskdfjlsakdjflaskdjflsakdfljaskdffffffffffffffffffffffffffffffff
+				{import.meta.env.MODE}
+			</h1>
+		</>
 	)
 }
---
+++ src/share/Notification.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../styles/notification-wrapper.css'
+
+export default function Notification({ message, status, isActive, inActive }){
+	return (
+		<div className={isActive ? 'notification-wrapper is-active':"notification-wrapper"}>
+			<div className={status ? `notification is-${status}`: 'notification is-success'}>
+				<button 
+					onClick={() => inActive()} 
+					className="delete">					
+				</button>
+				{message || "test notification !!!!"}
+			</div>
+		</div>
+	)
+}
